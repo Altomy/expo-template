589083be1ebdec283acdae783be18f89e6f1fb1c
Ahmed Altomy <93818544+Altomy@users.noreply.github.com>
2022-06-09 23:22:54 +0300

    Update app startup

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
+/* Importing the library */
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-const i = "";
+import { Text, View } from "react-native";
+import { useAppStartup } from "hooks";
+
+/**
+ * "If the app is not ready, return null, otherwise return the app."
+ *
+ * The app is not ready until the app startup function has completed.
+ *
+ * The app startup function is an asynchronous function that returns a boolean.
+ *
+ * @returns A function that returns null || JSX
+ */
 export default function App() {
+  /* Calling the useAppStartup() hook. */
+  const appIsReady = useAppStartup();
+
+  /* Returning null if the app is not ready. */
+  if (!appIsReady) return null;
+  /* Returning a view with a text and status bar. */
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 13 }}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
---
+++ src/hooks/useAppStartup.ts
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import {
+  Inter_900Black,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+
+/**
+ * This function will prevent the splash screen from auto hiding, load the fonts, and then hide the
+ * splash screen when the app is ready.
+ * @returns A boolean value.
+ */
+export function useAppStartup() {
+  /* Creating a state variable called appIsReady and setting it to false. */
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  /* A hook that is called when the component is mounted. */
+  useEffect(() => {
+    /**
+     * The function waits for the SplashScreen to prevent auto-hiding, then loads the fonts, then
+     * sets the appIsReady state to true, then hides the SplashScreen.
+     */
+    async function prepare() {
+      try {
+        /* Preventing the splash screen from auto hiding. */
+        await SplashScreen.preventAutoHideAsync();
+        /* Loading the fonts. */
+        await useFontsLoader();
+      } catch (error) {
+        /* Throwing an error if the app fails to load. */
+        throw new Error("App startup error" + error);
+      } finally {
+        /* Setting the state of the appIsReady variable to true. */
+        setAppIsReady(true);
+        /* Hiding the splash screen. */
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    /* Calling the function. */
+    prepare();
+  }, []);
+
+  /* Returning the state of the appIsReady variable. */
+  return appIsReady;
+}
+
+/**
+ * It loads the fonts
+ * @returns Nothing.
+ */
+async function useFontsLoader() {
+  /* Loading the fonts. */
+  await Font.loadAsync({
+    Inter_900Black,
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  /* Returning nothing. */
+  return;
+}
